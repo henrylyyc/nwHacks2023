606dbdb86b012c64a2bc310979c4e669f3a4e6c0
Henry <23145148+henrylyyc@users.noreply.github.com>
2023-01-21 19:17:32 -0800

    Functioning Playback Object in React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,46 @@
 import logo from './logo.svg';
+import { Player } from '@livepeer/react';
+import blenderPoster from './components/DSC_4813.jpg';
 import './App.css';
+import {
+  LivepeerConfig,
+  createReactClient,
+  studioProvider,
+} from '@livepeer/react';
+ 
+const client = createReactClient({
+  provider: studioProvider({ apiKey: '594fa6a7-bf96-459f-8adb-92232cc4c5eb' }),
+});
+
+const PosterImage = () => {
+  return (
+    null
+  );
+};
+
+function PlayerComponent() {
+  return (
+    <Player
+      title="Agent 327: Operation Barbershop"
+      playbackId="6d7el73r1y12chxr"
+      poster={<PosterImage />}
+      showPipButton
+      objectFit="cover"
+      priority
+    />
+  );
+};
 
 function App() {
   return (
+    <LivepeerConfig client={client}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <PlayerComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +51,7 @@ function App() {
         </a>
       </header>
     </div>
+    </LivepeerConfig>
   );
 }
 
